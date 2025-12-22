1b9106b79d85431ba35be9fad88d3e39c0cc19d9
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2025-12-22 21:25:02 +0100

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import { errorHandler } from "docta-package";
+import { errorHandler, logger } from "docta-package";
 import paymentRouter from "./routers/paymentRouter";
 
 import { swaggerSpec } from "./swagger";
@@ -9,6 +9,22 @@ import webhookHandler from "./routers/webhook";
 const app = express();
 
 app.use(express.json());
+
+// Request logger
+app.use((req, res, next) => {
+  logger.info(`Request: ${req.method} ${req.url}`, {
+    method: req.method,
+    route: req.path,
+    baseUrl: req.baseUrl,
+    originalUrl: req.originalUrl,
+    params: req.params,
+    query: req.query,
+    body: req.body,
+    headers: req.headers,
+    ip: req.ip,
+  });
+  next();
+});
 
 // Swagger documentation route
 app.use("/api/payment/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
